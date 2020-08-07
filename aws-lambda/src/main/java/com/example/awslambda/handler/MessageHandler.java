3b21750f4d11da9cdd25802a6a2a23281dada459
sumit <spsingh02497@gmail.com>
2020-08-07 09:28:07 +0530

    AWS Lambda Function Example

---
+++ aws-lambda/src/main/java/com/example/awslambda/handler/MessageHandler.java
@@ -0,0 +1,17 @@
+package com.example.awslambda.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+
+public class MessageHandler implements RequestHandler<Integer, String>{
+  
+  @Override
+  public String handleRequest(Integer event, Context context)
+  {
+    LambdaLogger logger = context.getLogger();
+    logger.log("EVENT Count: " + event.toString());
+    return event.toString();
+  }
+}
