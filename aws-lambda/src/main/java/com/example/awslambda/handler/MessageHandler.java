d7d232feba26c4b4c20e58785e105def13e0b480
sumit <spsingh02497@gmail.com>
2020-08-19 20:50:26 +0530

    added one more endpoint getMesssage()

--- aws-lambda/src/main/java/com/example/awslambda/handler/MessageHandler.java
+++ aws-lambda/src/main/java/com/example/awslambda/handler/MessageHandler.java
@@ -4,14 +4,16 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+public class MessageHandler implements RequestHandler<Integer, String> {
 
-public class MessageHandler implements RequestHandler<Integer, String>{
-  
-  @Override
-  public String handleRequest(Integer event, Context context)
-  {
-    LambdaLogger logger = context.getLogger();
-    logger.log("EVENT Count: " + event.toString());
-    return event.toString();
-  }
+	@Override
+	public String handleRequest(Integer event, Context context) {
+		LambdaLogger logger = context.getLogger();
+		logger.log("EVENT Count: " + event.toString());
+		return event.toString();
+	}
+
+	public String getMessage() {
+		return "getting response from Aws Api gateway.";
+	}
 }
